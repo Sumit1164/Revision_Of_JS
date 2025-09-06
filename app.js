5ec74c6be24d762ac4bf27c025f6c6e241fc3ac1
Sumit tripathi <sumittripathi8795@gmail.in>
2025-09-06 18:03:19 +0530

    Constructor method in js

--- app.js
+++ app.js
@@ -13,3 +13,30 @@ const student2 = {
     eachSubMarks: 87
 };
 student2.__proto__ = student;
+
+
+
+
+// Class i js
+
+class Car{
+    constructor(brand, mileage) {
+        console.log("Car object created");
+        this.brand = brand;
+        this.mileage = mileage;
+    }
+    start() {
+        console.log("Car started");
+    }
+    stop() {
+        console.log("Car stopped");
+    }
+    SetBrand(brand) {
+        this.brand = brand;
+    }
+}
+
+let car1 = new Car("Lamborghini", 7);
+console.log(car1);
+let car2 = new Car("Ferrari", 5);
+console.log(car2);
