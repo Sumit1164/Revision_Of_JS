483e076a00038f628ab8db68ef90971577bcaeee
Sumit tripathi <sumittripathi8795@gmail.in>
2025-09-29 19:11:32 +0530

    fix: update initial input value and enhance button click handling for currency conversion

--- Currency Converter-project/script.js
+++ Currency Converter-project/script.js
@@ -1,7 +1,11 @@
-const BASE_URL = "https://latest.currency-api.pages.dev/v1/currencies/usd.json";
+const BASE_URL =
+    "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/v1/currencies";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
-
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg")
 
 for (let select of dropdowns) {
     for (currCode in countryList) {
@@ -27,3 +31,21 @@ const updateFlag = (element) => {
     let img = element.parentElement.querySelector("img");
     img.src = newSrc;
 }
+
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault(); // baar baar page reload ho jata hai iske liye
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if (amtVal === "" || amtVal < 1) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+    // console.log(fromCurr.value, toCurr.value);
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[toCurr.value.toLowerCase()];
+    
+    let fAmount = amtVal * rate;
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${fAmount} ${toCurr.value}`;
+});
